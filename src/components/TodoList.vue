39bf35de15b881d5d79976304d644d9753319532
Valentin von Guttenberg <greengiraffe@posteo.de>
2018-07-12 12:07:22 +0200

    Enable sorting todos by name and due date

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,8 +1,15 @@
 <template>
-  <div>
-    <h2>ToDo</h2>
-    <ul class="todo-list">
-      
+  <div class="todo-list">
+    <div class="todo-list__header">
+      <h2 class="todo-list__title">ToDo</h2>
+      <label class="todo-list__sort-select">
+        Sort by
+        <select v-model="sortSelect" name="Sort todos" id="sort-todo-select">
+          <option v-for="option in sortOptions" :key="option.value" :value="option.value">{{ option.text }}</option>
+        </select>
+      </label>
+    </div>
+    <ul class="todo-list__list">
       <li v-for="todo in todos" :key="todo.id" class="todo-list__item">
         <Todo :todo="todo" />
       </li>
@@ -15,18 +22,56 @@
 import Todo from './Todo.vue'
 import TodoInput from './TodoInput.vue'
 
+const SORT_BY_NAME = 'SORT_BY_NAME'
+const SORT_BY_DATE_DESC = 'SORT_BY_DATE_DESC'
+const SORT_BY_DATE_ASC = 'SORT_BY_DATE_ASC'
+
 export default {
   components: { Todo, TodoInput },
+  data: function () {
+    return {
+      sortOptions: [
+        { text: 'Name', value: SORT_BY_NAME},
+        { text: 'Due date (descending)', value: SORT_BY_DATE_DESC},
+        { text: 'Due date (ascending)', value: SORT_BY_DATE_ASC},
+      ],
+      sortSelect: SORT_BY_NAME,
+    }
+  },
   computed: {
     todos () {
-      return this.$store.getters.sortedTodos
+      switch (this.sortSelect) {
+        case SORT_BY_NAME:
+          return this.$store.getters.sortedTodosByName
+        case SORT_BY_DATE_DESC:
+          return this.$store.getters.sortedTodosByDateDesc
+        case SORT_BY_DATE_ASC:
+          return this.$store.getters.sortedTodosByDateAsc
+      }
     }
   }
 }
 </script>
 
 <style lang="scss">
-.todo-list {
+.todo-list__header {
+  display: flex;
+  align-items: baseline;
+}
+
+.todo-list__title {
+  display: inline-block;
+}
+
+.todo-list__sort-select {
+  margin-left: auto;
+
+  select {
+    margin-left: 0.5rem;
+  }
+}
+
+.todo-list__list {
   margin: 0;
   padding: 0;
   list-style-type: none;
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -12,27 +12,29 @@ export default {
       { id: 2, text: 'Buy milk', important: true, done: false, due: undefined, category: 2 },
       { id: 3, text: 'Watch Batman', important: false, done: true, due: undefined, category: undefined },
       { id: 4, text: 'Watch Star Wars', important: false, done: true, due: undefined, category: undefined },
-      { id: 5, text: 'Watch Lord of the Rings', important: false, done: false, due: undefined, category: undefined }
+      { id: 5, text: 'Watch Lord of the Rings', important: false, done: false, due: undefined, category: undefined },
+      { id: 6, text: 'Buy spaghetti', important: false, done: false, due: new Date('2018-07-20T09:45:00'), category: undefined },
+      { id: 7, text: 'Do math homework', important: false, done: false, due: new Date('2018-07-18T09:45:00'), category: undefined },
+      { id: 8, text: 'Do a workout', important: true, done: false, due: undefined, category: undefined }
     ]
   },
   getters: {
     doneTodos: state => {
       return state.todos.filter(todo => todo.done)
     },
-    sortedTodos: state => {
-      return state.todos.sort((a, b) => {
-        // check if done
-        if (a.done && b.done) return 0
-        if (a.done && !b.done) return 1
-        if (!a.done && b.done) return -1
-        if (!(a.done || b.done)) {
-          // check if important
-          if (a.important && b.important) return a > b
-          if (a.important && !b.important) return -1
-          if (!a.important && b.important) return 1
-          if (!(a.important || b.important)) return 0
-        }
-      })
+    sortedTodosByName: state => {
+      let sorted = state.todos.sort(sortByText)
+      // sorted = sorted.sort(sortDoneToBottom)
+      // sorted = sorted.sort(sortImportantToTop)
+      return sorted
+    },
+    sortedTodosByDateDesc: state => {
+      let sorted = state.todos.sort(sortByDateDesc)
+      return sorted
+    },
+    sortedTodosByDateAsc: state => {
+      let sorted = state.todos.sort(sortByDateAsc)
+      return sorted
     }
   },
   mutations: {
@@ -55,4 +57,33 @@ export default {
   actions: {
     // actions for asynchronous mutations (e.g. API calls)
   }
+}
+
+function sortByDateDesc (a, b) {
+  if (!a.due && b.due) return 1
+  if (a.due && !b.due) return -1
+  if (a.due && b.due) return b.due - a.due
+  return 0
+}
+
+function sortByDateAsc (a, b) {
+  if (a.due && b.due) return a.due - b.due
+  return 0
+}
+
+function sortByText (a, b) {
+  return a.text.toLowerCase().localeCompare(b.text.toLowerCase())
+}
+
+function sortDoneToBottom (a, b) {
+  if ((a.done && b.done) || !(a.done || b.done)) return 0
+  if (a.done && !b.done) return 1
+  if (!a.done && b.done) return -1
+}
+
+function sortImportantToTop (a, b) {
+  if (a.important && b.important) return a > b
+  if (a.important && !b.important) return -1
+  if (!a.important && b.important) return 1
+  if (!(a.important || b.important)) return 0
 }
